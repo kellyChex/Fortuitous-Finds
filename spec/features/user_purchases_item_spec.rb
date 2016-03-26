4dda11910d31b26795092e244b8a608c5f6ab81f
KellyChess <chess.kelly@gmail.com>
2016-03-25 17:50:10 -0700

    complete user_purchases_item_spec

--- spec/features/user_purchases_item_spec.rb
+++ spec/features/user_purchases_item_spec.rb
@@ -1,15 +1,36 @@
 require 'rails_helper'
 
 RSpec.feature 'user purchases item' do
-  let!(:user) { create(:user) }
+  let!(:signed_in_user) { create(:user) }
+  let!(:user)           { create(:user) }
+  let!(:listing)        { create(:listing, user: user) }
+  let!(:listing2)        { create(:listing, user: user) }
+
+  background do
+    login_as(signed_in_user, scope: :user)
+  end
 
   context "from another users' listing" do
     it 'is successful' do
+      visit listing_path(listing)
+      click_on('Buy It Now')
+      fill_in 'Address', with: '1 Rainbow Rd'
+      fill_in 'City', with: 'San Diego'
+      fill_in 'State', with: 'CA'
+      click_on 'Complete Order'
+      expect(page).to have_content I18n.t('messages.created', name: 'Order')
     end
   end
 
   context "from own users' listing" do
     it 'is successful' do
+      visit listing_path(listing2)
+      click_on('Buy It Now')
+      fill_in 'Address', with: '1 Rainbow Rd'
+      fill_in 'City', with: 'San Diego'
+      fill_in 'State', with: 'CA'
+      click_on 'Complete Order'
+      expect(page).to have_content I18n.t('messages.created', name: 'Order')
     end
   end
 end
