f840e82ee386cd0bcd275c2b7127d30cdc710d57
KellyChess <chess.kelly@gmail.com>
2016-03-28 17:22:33 -0700

    add search method test to pages_controller_spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,15 @@ class PagesController < ApplicationController
   end
 
   def search
-    if !params[:search_string].nil?
+    if !params[:search_string].blank?
         search_string = params[:search_string].strip.downcase
         search_string = search_string.gsub("'", "\''")
-        @storage = Listing.joins(:user).where("LOWER(users.name) LIKE '%#{search_string}%' OR LOWER(listings.name) LIKE '%#{search_string}%'")
+        @search = Listing.joins(:user).where("LOWER(users.name)
+          LIKE '%#{search_string}%' OR LOWER(listings.name)
+            LIKE '%#{search_string}%'")
         render pages_search_path
+    else
+      redirect_to :back
     end
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  before :each do
+    @user = create(:user, name: 'Kat')
+    @user2 = create(:user, name: 'Billy')
+    @listing1 = create(:listing, name: 'Rug', user: @user)
+    @listing2 = create(:listing, name: 'Car', user: @user)
+    @user2_listing = create(:listing, name: 'Surfboard', user: @user2)
+    sign_in @user
+  end
 
+  describe 'GET #search' do
+    context 'with valid attributes' do
+      it 'assigns the search to @search' do
+        search = @search
+        get :search, @search
+        expect(assigns(:search_string)).to eq(search)
+      end
+
+      it 'renders the search view' do
+        search = @search
+        get :search, @search
+        expect(response).to render_template('pages/search', 'layouts/application')
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'assigns the search to @search' do
+        search = @search
+        get :search, @search
+        expect(assigns(:search_string)).to eq(search)
+      end
+
+      it 'renders the search view' do
+        search = @search
+        get :search, @search
+        expect(response).to render_template('pages/search', 'layouts/application')
+      end
+    end
+  end
 end
